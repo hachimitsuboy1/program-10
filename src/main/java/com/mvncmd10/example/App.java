74bb16865e2b967672843a7648d70c3a7ab14f1e
Prathama Nadiga <159523718+hachimitsuboy1@users.noreply.github.com>
2025-06-09 06:37:45 +0530

    Add files via upload

---
+++ src/main/java/com/mvncmd10/example/App.java
@@ -0,0 +1,13 @@
+package com.mvncmd10.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
